27755a4383092f945581fbaab4d53cf62a5bdd30
mkakh <akhs3214@gmail.com>
2025-02-17 10:34:05 +0900

    add a feature to calculate the average of speeds

--- tools/average_distances_and_speeds.py
+++ tools/average_distances_and_speeds.py
@@ -2,8 +2,9 @@ import os
 import yaml
 import numpy as np
 
-def calculate_distances(directory):
+def calculate(directory):
     distances = []
+    speeds = []
     for file in os.listdir(directory):
         if file.endswith(".yaml"):
             file_path = os.path.join(directory, file)
@@ -15,11 +16,13 @@ def calculate_distances(directory):
                         trajectory = test['test-' + str(id)]
                         initState = trajectory[0]
                         lastState = trajectory[-1]
-                        distances.append(lastState['ego']['x'] - initState['ego']['x'])
+                        distance = lastState['ego']['x'] - initState['ego']['x']
+                        distances.append(distance)
+                        speeds.append(distance / lastState['timeStamp'])
 
                 except yaml.YAMLError as exc:
                     print(exc)
-    return distances
+    return (distances, speeds)
 
 if __name__ == '__main__':
     import argparse
@@ -28,5 +31,9 @@ if __name__ == '__main__':
     parser.add_argument('directory', type=str, help='directory containing trajectory YAML files')
 
     args = parser.parse_args()
-    distances = np.array(calculate_distances(args.directory))
+    (distances, speeds) = map(np.array,calculate(args.directory))
+
+    print("Distances:")
     print(f"num: {len(distances)}, mean: {np.mean(distances)}, std: {np.std(distances)}, max: {np.max(distances)}, min: {np.min(distances)}")
+    print("Speeds:")
+    print(f"num: {len(speeds)}, mean: {np.mean(speeds)}, std: {np.std(speeds)}, max: {np.max(speeds)}, min: {np.min(speeds)}")
